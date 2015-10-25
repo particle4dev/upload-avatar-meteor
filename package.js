e149d099e6d18fe7da0096b93796fd6892297749
selaias <s.elaias@gmail.com>
2015-10-25 21:45:35 +0000

    upgrade to work with Meteor@1.2.0.2

--- package.js
+++ package.js
@@ -1,48 +1,46 @@
 Package.describe({
-    summary: "simple upload avatar package for meteor",
-    version: "1.1.0",
-    name: "particle4dev:upload-avatar",
-    git: "https://github.com/particle4dev/upload-avatar-meteor.git"    
+  summary: "simple upload avatar package for meteor",
+  version: "1.2.0",
+  name: "particle4dev:upload-avatar",
+  git: "https://github.com/particle4dev/upload-avatar-meteor.git"    
 });
 
-// meteor test-packages ./
 var both = ['client', 'server'];
 var client = ['client'];
 var server = ['server'];
 
-Package.on_use(function (api) {
-    api.versionsFrom("METEOR@0.9.3");
+Package.onUse(function (api) {
+  api.versionsFrom("METEOR@1.2.0.2");
 
-    api.use(['underscore', 'accounts-base', 'accounts-password'], both);
-    api.use(['jquery', 'templating', 'mizzao:bootstrap-3@3.2.0_1'], client);
+  api.use(['underscore', 'accounts-base', 'accounts-password'], both);
+  api.use(['jquery', 'templating', 'twbs:bootstrap'], client);
 
-    //add file
-    api.add_files([
-        'src/helpers.js'
-    ], both);
+  //add file
+  api.addFiles([
+    'src/helpers.js'
+  ], both);
 
-    api.add_files([
-        'src/vendor/imgareaselect/border-anim-h.gif',
-        'src/vendor/imgareaselect/border-anim-v.gif',
-        'src/vendor/imgareaselect/imgareaselect-animated.css',
-        'src/vendor/imgareaselect/jquery.imgareaselect.pack.js',
-        'src/client.js',
-        'src/template/editYourAvatarModal.html',
-        'src/template/editYourAvatarModal.js',
-        'src/template/editYourAvatarModal.css'
-    ], client);
+  api.addAssets([
+    'src/vendor/imgareaselect/border-anim-h.gif',
+    'src/vendor/imgareaselect/border-anim-v.gif',
+  ], client);
 
-    api.add_files([
-        'src/server.js',
-    ], server);
+  api.addFiles([
+    'src/vendor/imgareaselect/imgareaselect-animated.css',
+    'src/vendor/imgareaselect/jquery.imgareaselect.pack.js',
+    'src/client.js',
+    'src/template/editYourAvatarModal.html',
+    'src/template/editYourAvatarModal.js',
+    'src/template/editYourAvatarModal.css'
+  ], client);
 
-    if (typeof api.export !== 'undefined') {
-        //api.export('DEBUGX', both);
-    }
+  api.add_files([
+    'src/server.js',
+  ], server);
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use(['particle4dev:upload-avatar','accounts-password','tinytest'], both);
-  api.add_files('tests/client.js', 'client');
-  api.add_files('tests/server.js', 'server');
+  api.addFiles('tests/client.js', 'client');
+  api.addFiles('tests/server.js', 'server');
 });
